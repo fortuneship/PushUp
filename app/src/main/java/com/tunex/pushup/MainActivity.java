ff68a23a53c86610d09064ccc06f991ca0000cfd
fortuneship <tunde8983@gmail.com>
2017-12-10 04:59:08 +0100

    Modified MainActivity Added New File 'TestAddFile.java'

---
+++ app/src/main/java/com/tunex/pushup/MainActivity.java
@@ -0,0 +1,21 @@
+package com.tunex.pushup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void testUpload(){
+
+        int i = 1+1;
+        int ii = 2+2;
+
+        int iii = i + ii;
+    }
+}
---
+++ app/src/main/java/com/tunex/pushup/TestAddFile.java
@@ -0,0 +1,8 @@
+package com.tunex.pushup;
+
+/**
+ * Created by hp on 10-Dec-17.
+ */
+
+public class TestAddFile {
+}
